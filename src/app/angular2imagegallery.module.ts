f9ed321f56617ac041ba396060d4328a18d71bc5
slobodansekulic <slobx13@gmail.com>
2018-01-02 16:50:04 +0100

    Removing BS Animation Inclusion

--- src/app/angular2imagegallery.module.ts
+++ src/app/angular2imagegallery.module.ts
@@ -2,13 +2,11 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ImageService} from "./services/image.service";
 import {GalleryComponent} from "./gallery/gallery.component";
 import {ViewerComponent} from "./viewer/viewer.component";
 import {DemoComponent} from "./demo/demo.component";
 import {CommonModule} from "@angular/common";
-
 
 @NgModule({
   declarations: [
@@ -19,8 +17,7 @@ import {CommonModule} from "@angular/common";
   imports: [
     CommonModule,
     FormsModule,
-    HttpModule,
-    BrowserAnimationsModule
+    HttpModule
   ],
   providers: [
     ImageService
